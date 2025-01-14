205455b0bd0d1fc81cb93701c5a067416de675a9
Gleb Savin <--@--.-->
2025-01-14 15:06:12 +0300

    add base project

---
+++ Strategies/DeltaStatistic/DeltaStatistic/DeltaStatistic.cs
@@ -0,0 +1,14 @@
+﻿//Example from https://atas.net/ru/obemnyj-analiz/strategii-i-torgovye-patterny/prohrammyrovanye/
+
+using ATAS.Indicators;
+
+namespace DeltaStatistic
+{
+    public class DeltaStatistic : Indicator
+    {
+        protected override void OnCalculate(int bar, decimal value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
